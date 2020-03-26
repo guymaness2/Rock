99c2a377ff386119682bb42c47719c7dd38cbe70
Matt Bronkema <matt.bronkema@sparkdevnetwork.org>
2020-03-26 13:55:13 -0400

    - Initial commit of Logger singleton class.

---
+++ Rock.Tests.Integration/Utility/LoggerTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rock.Utility;
+using System.Threading.Tasks;
+
+namespace Rock.Tests.Integration.Utility
+{
+    /// <summary>
+    /// Summary description for LoggerTests
+    /// </summary>
+    [TestClass]
+    public class LoggerTests
+    {
+        public LoggerTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public async Task LogInformationWorks()
+        {
+            Logger.Log.Information( "This is a test." );
+
+            Logger.Log.Information( "coolPlugin", "This is a test." );
+
+            var domainLogs = ProcessDomainLogs( 20, "This is a domain test" );
+            var logs = ProcessLogs( 10, "This is a test" );
+
+            await logs;
+            await domainLogs;
+        }
+
+        private async Task ProcessDomainLogs( int logCount, string logMessage )
+        {
+            var taskList = new List<Task>();
+
+            for ( var i = 0; i < logCount; i++ )
+            {
+                taskList.Add(Task.Run( () =>
+                {
+                    Logger.Log.Information( "coolPlugin", $"{logMessage} {i}." );
+                } ));
+            }
+            var t = Task.WhenAll( taskList.ToArray() );
+            await t;
+        }
+
+        private async Task ProcessLogs( int logCount, string logMessage )
+        {
+            var taskList = new List<Task>();
+
+            for ( var i = 0; i < logCount; i++ )
+            {
+                taskList.Add( Task.Run( () =>
+                {
+                    Logger.Log.Information( $"{logMessage} {i}." );
+                } ) );
+            }
+            var t = Task.WhenAll( taskList.ToArray() );
+            await t;
+        }
+    }
+}
---
+++ Rock/Utility/Logger.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace Rock.Utility
+{
+    public class Logger
+    {
+        private static Logger log;
+        public static Logger Log {
+            get
+            {
+                if(log == null )
+                {
+                    log = new Logger();
+                }
+                return log;
+            }
+        }
+
+        private Logger()
+        {
+           Serilog.Log.Logger = new LoggerConfiguration()
+                .MinimumLevel
+                .Verbose()
+                .WriteTo.File( "logs\\rock.log",
+                    rollingInterval: RollingInterval.Day,
+                    buffered: true,
+                    shared: false,                    
+                    restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                    retainedFileCountLimit: 1,
+                    rollOnFileSizeLimit: true,
+                    fileSizeLimitBytes: 5000000)
+                .CreateLogger();
+        }
+
+        ~Logger()
+        {
+            Serilog.Log.CloseAndFlush();
+        }
+
+        public void Information(string message)
+        {
+            Serilog.Log.Logger.Information( message );
+            Debug.WriteLine( message );
+        }
+
+        public void Information( string domain, string message )
+        {
+            Serilog.Log.Logger.Information("{domain} {message}", domain, message );
+            Debug.WriteLine( $"{domain} {message}" );
+        }
+    }
+}

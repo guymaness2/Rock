a8822ce3fd2277ec320a7f7ed11e5a34d8f29a2b
Matt Bronkema <matt.bronkema@sparkdevnetwork.org>
2020-03-27 14:09:14 -0400

    - Add logging methods.

--- Rock.Tests.Integration/Utility/LoggerTests.cs
+++ Rock.Tests.Integration/Utility/LoggerTests.cs
@@ -7,18 +7,92 @@ using System.Threading.Tasks;
 
 namespace Rock.Tests.Integration.Utility
 {
+    public static class AssertExtensions
+    {
+        public static void FileContains( this Assert assert, string filePath, string expectedString )
+        {
+            if ( string.IsNullOrWhiteSpace( filePath ) )
+            {
+                throw new ArgumentException( "No file path provided.", "filePath" );
+            }
+
+            if ( string.IsNullOrWhiteSpace( expectedString ) )
+            {
+                throw new ArgumentException( "No expected string provided.", "expectedString" );
+            }
+
+            var file = System.IO.File.ReadAllText( filePath );
+            var fileContainsExpectedString = file.Contains( expectedString );
+
+            if ( !fileContainsExpectedString )
+            {
+                throw new AssertFailedException( $"File {filePath} did not contain '{expectedString}'." );
+            }
+
+        }
+
+        public static void FileDoesNotContains( this Assert assert, string filePath, string excludedString )
+        {
+            if ( string.IsNullOrWhiteSpace( filePath ) )
+            {
+                throw new ArgumentException( "No file path provided.", "filePath" );
+            }
+
+            if ( string.IsNullOrWhiteSpace( excludedString ) )
+            {
+                throw new ArgumentException( "No exclude string provided.", "excludedString" );
+            }
+
+            var file = System.IO.File.ReadAllText( filePath );
+            var fileContainsExpectedString = file.Contains( excludedString );
+            if ( fileContainsExpectedString )
+            {
+                throw new AssertFailedException( $"File {filePath} contain '{excludedString}'." );
+            }
+        }
+
+        public static void FolderHasCorrectNumberOfFiles( this Assert assert, string folderPath, int fileCount )
+        {
+            if ( string.IsNullOrWhiteSpace( folderPath ) )
+            {
+                throw new ArgumentException( "No folder path provided.", "folderPath" );
+            }
+
+            var folder = System.IO.Directory.GetFiles( folderPath );
+            var folderHasCorrectNumberOfFiles = folder.Length == fileCount;
+            if ( !folderHasCorrectNumberOfFiles )
+            {
+                throw new AssertFailedException( $"The folder {folderPath} contain {folder.Length} files but expected {fileCount} files." );
+            }
+        }
+    }
+
     /// <summary>
     /// Summary description for LoggerTests
     /// </summary>
     [TestClass]
     public class LoggerTests
     {
+        private const string LOG_PATH = "logs\\rock.log";
+
         class RockLogConfiguration : IRockLogConfiguration
         {
             public RockLogLevel LogLevel { get; set; }
             public int MaxFileSize { get; set; }
             public int NumberOfLogFiles { get; set; }
             public List<string> DomainsToLog { get; set; }
+            public string LogPath { get; set; }
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            var logFolderPath = System.IO.Path.GetFullPath( System.IO.Path.GetDirectoryName( LOG_PATH ) );
+            var files = System.IO.Directory.GetFiles( logFolderPath );
+            foreach(var file in files )
+            {
+                System.IO.File.Delete( file );
+            }
         }
 
         [TestMethod]
@@ -29,11 +103,131 @@ namespace Rock.Tests.Integration.Utility
                 LogLevel = RockLogLevel.All,
                 MaxFileSize = 1,
                 NumberOfLogFiles = 1,
-                DomainsToLog = new List<string> { "OTHER" }
+                DomainsToLog = new List<string> { "OTHER" },
+                LogPath = LOG_PATH
             };
             var logger = new Logger( config );
-            logger.Information( "Test" );
-            logger.Information( "CRM", "CRM Test" );
+
+            var expectedLogMessage = $"Test - {Guid.NewGuid()}";
+            var excludedLogMessage = $"CRM Test - {Guid.NewGuid()}";
+
+            logger.Information( expectedLogMessage );
+            logger.Information( "CRM", excludedLogMessage );
+            logger.Close();
+
+            Assert.That.FileContains( config.LogPath, expectedLogMessage );
+            Assert.That.FileDoesNotContains( config.LogPath, excludedLogMessage );
+        }
+
+        [TestMethod]
+        public void LogInformationShouldDomainsShouldBeCaseInsensitive()
+        {
+            var config = new RockLogConfiguration
+            {
+                LogLevel = RockLogLevel.All,
+                MaxFileSize = 1,
+                NumberOfLogFiles = 1,
+                DomainsToLog = new List<string> { "OTHER", "crm" },
+                LogPath = LOG_PATH
+            };
+            var logger = new Logger( config );
+
+            var expectedLogMessage = $"Test - {Guid.NewGuid()}";
+            var excludedLogMessage = $"CRM Test - {Guid.NewGuid()}";
+
+            logger.Information( expectedLogMessage );
+            logger.Information( "CRM", excludedLogMessage );
+            logger.Close();
+
+            Assert.That.FileContains( config.LogPath, expectedLogMessage );
+            Assert.That.FileContains( config.LogPath, excludedLogMessage );
+        }
+
+        [TestMethod]
+        public void LoggerShouldKeepOnlyMaxFileCount()
+        {
+            var config = new RockLogConfiguration
+            {
+                LogLevel = RockLogLevel.All,
+                MaxFileSize = 1,
+                NumberOfLogFiles = 2,
+                DomainsToLog = new List<string> { "OTHER", "crm" },
+                LogPath = LOG_PATH
+            };
+            var logger = new Logger( config );
+
+
+            var maxByteCount = config.MaxFileSize * 1024 * 1024 * ( config.NumberOfLogFiles + 2 );
+            var currentByteCount = 0;
+            while ( currentByteCount < maxByteCount )
+            {
+                var expectedLogMessage = $"Test - {Guid.NewGuid()}";
+                logger.Information( expectedLogMessage );
+
+                currentByteCount += Encoding.ASCII.GetByteCount( expectedLogMessage );
+            }
+
+            logger.Close();
+            var logFolderPath = System.IO.Path.GetFullPath( System.IO.Path.GetDirectoryName( config.LogPath ) );
+            Assert.That.FolderHasCorrectNumberOfFiles( logFolderPath, config.NumberOfLogFiles );
+        }
+
+        [TestMethod]
+        public void LoggerVerboseShouldLogDomainCorrectly()
+        {
+            var config = new RockLogConfiguration
+            {
+                LogLevel = RockLogLevel.All,
+                MaxFileSize = 1,
+                NumberOfLogFiles = 2,
+                DomainsToLog = new List<string> { "OTHER", "crm" },
+                LogPath = LOG_PATH
+            };
+            var logger = new Logger( config );
+
+            var objectParams = new Object[] { config, RockLogLevel.All };
+            var testException = new Exception( "Test Exception" );
+
+            logger.Verbose( $"{Guid.NewGuid()}" );
+            logger.Verbose( "CRM", $"{Guid.NewGuid()}" );
+
+            logger.Verbose( $"{Guid.NewGuid()} {{@oneProperty}} {{@twoProperty}}", config, RockLogLevel.All );
+            logger.Verbose( "CRM", $"{Guid.NewGuid()} {{@oneProperty}} {{@twoProperty}}", config, RockLogLevel.All );
+
+            logger.Verbose( testException, $"{Guid.NewGuid()}" );
+            logger.Verbose( "CRM", testException, $"{Guid.NewGuid()}" );
+
+            logger.Verbose( testException, $"{Guid.NewGuid()} {{@oneProperty}} {{@twoProperty}}", config, RockLogLevel.All );
+            logger.Verbose( "CRM", testException, $"{Guid.NewGuid()} {{@oneProperty}} {{@twoProperty}}", config, RockLogLevel.All );
+        }
+
+        [TestMethod]
+        public void LoggerDebugShouldLogDomainCorrectly()
+        {
+            var config = new RockLogConfiguration
+            {
+                LogLevel = RockLogLevel.All,
+                MaxFileSize = 1,
+                NumberOfLogFiles = 2,
+                DomainsToLog = new List<string> { "OTHER", "crm" },
+                LogPath = LOG_PATH
+            };
+            var logger = new Logger( config );
+
+            var objectParams = new Object[] { config, RockLogLevel.All };
+            var testException = new Exception( "Test Exception" );
+
+            logger.Debug( $"{Guid.NewGuid()}" );
+            logger.Debug( "CRM", $"{Guid.NewGuid()}" );
+
+            logger.Debug( $"{Guid.NewGuid()} {{@oneProperty}} {{@twoProperty}}", config, RockLogLevel.All );
+            logger.Debug( "CRM", $"{Guid.NewGuid()} {{@oneProperty}} {{@twoProperty}}", config, RockLogLevel.All );
+
+            logger.Debug( testException, $"{Guid.NewGuid()}" );
+            logger.Debug( "CRM", testException, $"{Guid.NewGuid()}" );
+
+            logger.Debug( testException, $"{Guid.NewGuid()} {{@oneProperty}} {{@twoProperty}}", config, RockLogLevel.All );
+            logger.Debug( "CRM", testException, $"{Guid.NewGuid()} {{@oneProperty}} {{@twoProperty}}", config, RockLogLevel.All );
         }
 
         [TestMethod]
@@ -57,7 +251,7 @@ namespace Rock.Tests.Integration.Utility
             for ( var i = 0; i < logCount; i++ )
             {
                 var index = i;
-                taskList.Add(Task.Run( () => Logger.Log.Information( "coolPlugin", $"{logMessage} {index}." )));
+                taskList.Add( Task.Run( () => Logger.Log.Information( "coolPlugin", $"{logMessage} {index}." ) ) );
             }
             var t = Task.WhenAll( taskList.ToArray() );
             await t;
@@ -78,6 +272,5 @@ namespace Rock.Tests.Integration.Utility
             var t = Task.WhenAll( taskList.ToArray() );
             await t;
         }
-
     }
 }
--- Rock/Utility/Logger.cs
+++ Rock/Utility/Logger.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Serilog;
 using Serilog.Context;
+using Serilog.Core;
+using Serilog.Events;
 
 namespace Rock.Utility
 {
@@ -26,6 +28,7 @@ namespace Rock.Utility
         int MaxFileSize { get; set; }
         int NumberOfLogFiles { get; set; }
         List<string> DomainsToLog { get; set; }
+        string LogPath { get; set; }
     }
 
     internal class RockLogConfiguration : IRockLogConfiguration
@@ -34,6 +37,8 @@ namespace Rock.Utility
         public int MaxFileSize { get; set; }
         public int NumberOfLogFiles { get; set; }
         public List<string> DomainsToLog { get; set; }
+
+        public string LogPath { get; set; }
 
         // TODO: Get from database
         //public RockLogConfiguration()
@@ -46,6 +51,8 @@ namespace Rock.Utility
 
     public class Logger
     {
+        private const string DEFAULT_DOMAIN = "OTHER";
+
         private IRockLogConfiguration rockLogConfiguration;
         private static Logger log;
         public static Logger Log
@@ -71,7 +78,7 @@ namespace Rock.Utility
                  .MinimumLevel
                  .Verbose()
                  .WriteTo
-                 .File( "logs\\rock.log",
+                 .File( rockLogConfiguration.LogPath,
                      rollingInterval: RollingInterval.Infinite,
                      buffered: true,
                      shared: false,
@@ -87,16 +94,6 @@ namespace Rock.Utility
         ~Logger()
         {
             Serilog.Log.CloseAndFlush();
-        }
-
-        public void Information( string message )
-        {
-            Information( "Other", message );
-        }
-
-        public void Information( string domain, string message )
-        {
-            Serilog.Log.Logger.Information( "{domain} {message}", domain.ToUpper(), message );
         }
 
         private Serilog.Events.LogEventLevel GetLogEventLevelFromRockLogLevel( RockLogLevel logLevel )
@@ -125,11 +122,280 @@ namespace Rock.Utility
                 return false;
             }
             var domain = e.Properties["domain"] as Serilog.Events.ScalarValue;
-            if( domain == null )
+            if ( domain == null )
             {
                 return false;
             }
-            return rockLogConfiguration.DomainsToLog.Contains( domain.Value );
+            return rockLogConfiguration.DomainsToLog.Contains( domain.Value.ToString(), StringComparer.InvariantCultureIgnoreCase );
         }
+
+        public void Close()
+        {
+            Serilog.Log.CloseAndFlush();
+        }
+
+        private string GetMessageTemplateWithDomain( string messageTemplate )
+        {
+            return $"{{domain}} {messageTemplate}";
+        }
+
+        private object[] AddDomainToObjectArray( object[] propertyValues, string domain )
+        {
+            var properties = new List<object>( propertyValues );
+            properties.Insert( 0, domain );
+            return properties.ToArray();
+        }
+
+        #region Verbose
+        public void Verbose( string messageTemplate )
+        {
+            Verbose( DEFAULT_DOMAIN, messageTemplate );
+        }
+
+        public void Verbose( string domain, string messageTemplate )
+        {
+            Serilog.Log.Logger.Verbose( GetMessageTemplateWithDomain( messageTemplate ), domain.ToUpper() );
+        }
+
+        public void Verbose( string messageTemplate, params object[] propertyValues )
+        {
+            Verbose( DEFAULT_DOMAIN, messageTemplate, propertyValues );
+        }
+
+        public void Verbose( string domain, string messageTemplate, params object[] propertyValues )
+        {
+            Serilog.Log.Logger.Verbose( GetMessageTemplateWithDomain( messageTemplate ), AddDomainToObjectArray( propertyValues, domain.ToUpper() ) );
+        }
+
+        public void Verbose( Exception exception, string messageTemplate )
+        {
+            Verbose( DEFAULT_DOMAIN, exception, messageTemplate );
+        }
+
+        public void Verbose( string domain, Exception exception, string messageTemplate )
+        {
+            Serilog.Log.Logger.Verbose( exception, GetMessageTemplateWithDomain( messageTemplate ), domain.ToUpper() );
+        }
+
+        public void Verbose( Exception exception, string messageTemplate, params object[] propertyValues )
+        {
+            Verbose( DEFAULT_DOMAIN, exception, messageTemplate, propertyValues );
+        }
+
+        public void Verbose( string domain, Exception exception, string messageTemplate, params object[] propertyValues )
+        {
+            Serilog.Log.Logger.Verbose( exception, GetMessageTemplateWithDomain( messageTemplate ), AddDomainToObjectArray( propertyValues, domain.ToUpper() ) );
+        }
+        #endregion
+
+        #region Debug Methods
+
+        public void Debug( string messageTemplate )
+        {
+            Debug( DEFAULT_DOMAIN, messageTemplate );
+        }
+
+        public void Debug( string domain, string messageTemplate )
+        {
+            Serilog.Log.Logger.Debug( GetMessageTemplateWithDomain( messageTemplate ), domain.ToUpper() );
+        }
+
+        public void Debug( string messageTemplate, params object[] propertyValues )
+        {
+            Debug( DEFAULT_DOMAIN, messageTemplate, propertyValues );
+        }
+
+        public void Debug( string domain, string messageTemplate, params object[] propertyValues )
+        {
+            Serilog.Log.Logger.Debug( GetMessageTemplateWithDomain( messageTemplate ), AddDomainToObjectArray( propertyValues, domain.ToUpper() ) );
+        }
+        
+        public void Debug( Exception exception, string messageTemplate )
+        {
+            Debug( DEFAULT_DOMAIN, exception, messageTemplate );
+        }
+
+        public void Debug( string domain, Exception exception, string messageTemplate )
+        {
+            Serilog.Log.Logger.Debug( exception, GetMessageTemplateWithDomain( messageTemplate ), domain.ToUpper() );
+        }
+                
+        public void Debug( Exception exception, string messageTemplate, params object[] propertyValues )
+        {
+            Debug( DEFAULT_DOMAIN, exception, messageTemplate, propertyValues );
+        }
+
+        public void Debug(string domain, Exception exception, string messageTemplate, params object[] propertyValues )
+        {
+            Serilog.Log.Logger.Debug( exception, GetMessageTemplateWithDomain( messageTemplate ), AddDomainToObjectArray( propertyValues, domain.ToUpper() ) );
+        }
+        #endregion
+
+        #region Information Methods
+        public void Information<T>( string messageTemplate )
+        {
+            Information( DEFAULT_DOMAIN, messageTemplate );
+        }
+
+        public void Information( string messageTemplate, params object[] propertyValues )
+        {
+            Information( DEFAULT_DOMAIN, messageTemplate, propertyValues );
+        }
+
+        public void Information( Exception exception, string messageTemplate )
+        {
+            Information( DEFAULT_DOMAIN, exception, messageTemplate );
+        }
+
+        public void Information( Exception exception, string messageTemplate, params object[] propertyValues )
+        {
+            Information( DEFAULT_DOMAIN, exception, messageTemplate, propertyValues );
+        }
+
+        public void Information<T>(string domain, string messageTemplate)
+        {
+            Serilog.Log.Logger.Information( GetMessageTemplateWithDomain( messageTemplate ), domain.ToUpper() );
+        }
+
+        public void Information( string domain, string messageTemplate, params object[] propertyValues )
+        {
+            Serilog.Log.Logger.Information( GetMessageTemplateWithDomain( messageTemplate ), AddDomainToObjectArray( propertyValues, domain.ToUpper() ) );
+        }
+
+        public void Information( string domain, Exception exception, string messageTemplate )
+        {
+            Serilog.Log.Logger.Information( exception, GetMessageTemplateWithDomain( messageTemplate ), domain.ToUpper() );
+        }
+
+        public void Information( string domain, Exception exception, string messageTemplate, params object[] propertyValues )
+        {
+            Serilog.Log.Logger.Information( exception, GetMessageTemplateWithDomain( messageTemplate ), AddDomainToObjectArray( propertyValues, domain.ToUpper() ) );
+        }
+        #endregion
+
+        #region Warning Methods
+        public void Warning( string messageTemplate )
+        {
+            Warning( DEFAULT_DOMAIN, messageTemplate );
+        }
+
+        public void Warning( string messageTemplate, params object[] propertyValues )
+        {
+            Warning( DEFAULT_DOMAIN, messageTemplate, propertyValues );
+        }
+
+        public void Warning( Exception exception, string messageTemplate )
+        {
+            Warning( DEFAULT_DOMAIN, exception, messageTemplate );
+        }
+
+        public void Warning( Exception exception, string messageTemplate, params object[] propertyValues )
+        {
+            Warning( DEFAULT_DOMAIN, exception, messageTemplate, propertyValues );
+        }
+
+        public void Warning(string domain, string messageTemplate )
+        {
+            Serilog.Log.Logger.Warning( GetMessageTemplateWithDomain( messageTemplate ), domain.ToUpper() );
+        }
+
+        public void Warning( string domain, string messageTemplate, params object[] propertyValues )
+        {
+            Serilog.Log.Logger.Warning( GetMessageTemplateWithDomain( messageTemplate ), AddDomainToObjectArray( propertyValues, domain.ToUpper() ) );
+        }
+
+        public void Warning( string domain, Exception exception, string messageTemplate )
+        {
+            Serilog.Log.Logger.Warning( exception, GetMessageTemplateWithDomain( messageTemplate ), domain.ToUpper() );
+        }
+
+        public void Warning( string domain, Exception exception, string messageTemplate, params object[] propertyValues )
+        {
+            Serilog.Log.Logger.Warning( exception, GetMessageTemplateWithDomain( messageTemplate ), AddDomainToObjectArray( propertyValues, domain.ToUpper() ) );
+        }
+        #endregion
+
+        #region Error Methods
+        public void Error( string messageTemplate )
+        {
+            Error( DEFAULT_DOMAIN, messageTemplate );
+        }
+
+        public void Error( string messageTemplate, params object[] propertyValues )
+        {
+            Error( DEFAULT_DOMAIN, messageTemplate, propertyValues );
+        }
+
+        public void Error( Exception exception, string messageTemplate )
+        {
+            Error( DEFAULT_DOMAIN, exception, messageTemplate );
+        }
+
+        public void Error( Exception exception, string messageTemplate, params object[] propertyValues )
+        {
+            Error( DEFAULT_DOMAIN, exception, messageTemplate, propertyValues );
+        }
+
+        public void Error(string domain, string messageTemplate )
+        {
+            Serilog.Log.Logger.Error( GetMessageTemplateWithDomain( messageTemplate ), domain.ToUpper() );
+        }
+
+        public void Error( string domain, string messageTemplate, params object[] propertyValues )
+        {
+            Serilog.Log.Logger.Error( GetMessageTemplateWithDomain( messageTemplate ), AddDomainToObjectArray( propertyValues, domain.ToUpper() ) );
+        }
+
+        public void Error( string domain, Exception exception, string messageTemplate )
+        {
+            Serilog.Log.Logger.Error( exception, GetMessageTemplateWithDomain( messageTemplate ), domain.ToUpper() );
+        }
+
+        public void Error( string domain, Exception exception, string messageTemplate, params object[] propertyValues )
+        {
+            Serilog.Log.Logger.Error( exception, GetMessageTemplateWithDomain( messageTemplate ), AddDomainToObjectArray( propertyValues, domain.ToUpper() ) );
+        }
+        #endregion
+
+        #region Fatal Methods
+        public void Fatal( string messageTemplate )
+        {
+            Fatal( DEFAULT_DOMAIN, messageTemplate );
+        }
+
+        public void Fatal( string messageTemplate, params object[] propertyValues )
+        {
+            Fatal( DEFAULT_DOMAIN, messageTemplate, propertyValues );
+        }
+
+        public void Fatal( Exception exception, string messageTemplate )
+        {
+            Fatal( DEFAULT_DOMAIN, exception, messageTemplate );
+        }
+
+        public void Fatal( Exception exception, string messageTemplate, params object[] propertyValues )
+        {
+            Fatal( DEFAULT_DOMAIN, exception, messageTemplate, propertyValues );
+        }
+        public void Fatal(string domain, string messageTemplate )
+        {
+            Serilog.Log.Logger.Fatal( GetMessageTemplateWithDomain( messageTemplate ), domain.ToUpper() );
+        }
+
+        public void Fatal( string domain, string messageTemplate, params object[] propertyValues )
+        {
+            Serilog.Log.Logger.Fatal( GetMessageTemplateWithDomain( messageTemplate ), AddDomainToObjectArray( propertyValues, domain.ToUpper() ) );
+        }
+
+        public void Fatal( string domain, Exception exception, string messageTemplate )
+        {
+            Serilog.Log.Logger.Fatal( exception, GetMessageTemplateWithDomain( messageTemplate ), domain.ToUpper() );
+        }
+
+        public void Fatal( string domain, Exception exception, string messageTemplate, params object[] propertyValues )
+        {
+            Serilog.Log.Logger.Fatal( exception, GetMessageTemplateWithDomain( messageTemplate ), AddDomainToObjectArray( propertyValues, domain.ToUpper() ) );
+        }
+        #endregion
     }
 }

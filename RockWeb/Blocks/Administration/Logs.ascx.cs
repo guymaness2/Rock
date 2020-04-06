ed258e2245f4551693ef7e5055b37d45353a32ff
Matt Bronkema <matt.bronkema@sparkdevnetwork.org>
2020-04-06 15:06:41 -0400

    - Added log viewer bock.

---
+++ RockWeb/Blocks/Administration/Logs.ascx.cs
@@ -0,0 +1,148 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Extension;
+using Rock.Logging;
+using Rock.Security;
+using Rock.Web.Cache;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Blocks.Administration
+{
+    /// <summary>
+    /// Used to view the <see cref="Rock.Logging.RockLogEvent"/> logged via RockLogger.
+    /// </summary>
+    [System.ComponentModel.DisplayName( "Logs" )]
+    [System.ComponentModel.Category( "Administration" )]
+    [System.ComponentModel.Description( "Block to view system logs." )]
+
+    public partial class Logs : RockBlock, ICustomGridColumns
+    {
+        #region Control Methods
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            rGrid.GridRebind += rGrid_GridRebind;
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if ( !Page.IsPostBack )
+            {
+                BindGrid();
+            }
+        }
+        #endregion
+
+        #region Grid Events
+
+        /// <summary>
+        /// Handles the ApplyFilterClick event of the fDevice control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+        protected void rFilter_ApplyFilterClick( object sender, EventArgs e )
+        {
+            BindGrid();
+        }
+
+        /// <summary>
+        /// Handles the GridRebind event of the rGrid control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        protected void rGrid_GridRebind( object sender, EventArgs e )
+        {
+            BindGrid();
+        }
+
+        #endregion
+
+        #region Internal Methods
+
+        /// <summary>
+        /// Binds the grid.
+        /// </summary>
+        private void BindGrid()
+        {
+            var logReader = Rock.Logging.RockLogger.LogReader;
+            var logIndex = rGrid.PageIndex * rGrid.PageSize;
+
+            var events = logReader.GetEvents( logIndex, rGrid.PageSize );
+            if(logIndex > 0 )
+            {
+                rGrid.EmptyDataText = "The end of the log entries have been reached. <a href=''>Start Over.</a>";
+            }
+            var logEventModels = new List<RockLogEventModel>();
+            for(var i = 0; i < events.Count; i++ )
+            {
+                logEventModels.Add( new RockLogEventModel
+                {
+                    Id = i,
+                    DateTime = events[i].DateTime,
+                    Domain = events[i].Domain,
+                    Exception = events[i].Exception,
+                    Level = events[i].Level,
+                    Message = events[i].Message,
+                } );
+            }
+            rGrid.VirtualItemCount = logReader.RecordCount;
+            rGrid.DataSource = logEventModels;
+            rGrid.DataBind();
+        }
+        #endregion
+    }
+
+    internal class RockLogEventModel : RockLogEvent
+    {
+        public int Id { get; set; }
+        public string SerializedException
+        {
+            get
+            {
+                if(Exception == null )
+                {
+                    return string.Empty;
+                }
+
+                return Exception.ToString();
+            }
+        }
+    }
+}
